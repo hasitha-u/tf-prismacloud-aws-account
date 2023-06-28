b5668dd368597851f1d8ad2c2f52981599c7f067
Hasitha Ubeywarna <36988425+hasitha-u@users.noreply.github.com>
2023-06-28 22:26:49 +0100

    refactor

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-
+  # ARNs of managed policies to be attached to Prisma Cloud Role
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/SecurityAudit",
   ]
@@ -38,7 +38,7 @@ EOF
 
 }
 
-#Config scan capability is enabled by default
+# Config Scan policeis contains permissions required for Misconfigurations(CSPM), Identity Security, Threat Detection features which are is enabled by default
 resource "aws_iam_policy" "config_scan" {
   for_each = { for path in fileset(path.module, "policies/ConfigScan/*.json") : "${var.role_name_prefix}${split(".", basename(path))[0]}" => path }
 
@@ -49,14 +49,7 @@ resource "aws_iam_policy" "config_scan" {
   tags        = var.tags
 }
 
-
-resource "aws_iam_role_policy_attachment" "managed_policy" {
-  for_each = { for arn in toset(local.managed_policy_arns) : "AWSManaged-${split("/", arn)[1]}" => arn }
-
-  role       = aws_iam_role.this.name
-  policy_arn = each.value
-}
-
+# Permissions required for the `Agentless` feature
 resource "aws_iam_policy" "agentless" {
   for_each = contains(local.features, "Agentless Scanning") ? { for path in fileset(path.module, "policies/AgentlessScanning/*.json") : "${var.role_name_prefix}${split(".", basename(path))[0]}" => path } : {}
 
@@ -67,6 +60,7 @@ resource "aws_iam_policy" "agentless" {
   tags        = var.tags
 }
 
+# Permissions required for the `Auto Protect (Agent-Based Workload Protection)` feature
 resource "aws_iam_policy" "auto_protect" {
   for_each = contains(local.features, "Auto Protect") ? { for path in fileset(path.module, "policies/AutoProtect/*.json") : "${var.role_name_prefix}${split(".", basename(path))[0]}" => path } : {}
 
@@ -77,6 +71,7 @@ resource "aws_iam_policy" "auto_protect" {
   tags        = var.tags
 }
 
+# Permissions required for the `Data Security` feature
 resource "aws_iam_policy" "data_security" {
   for_each = contains(local.features, "Data Security") ? { for path in fileset(path.module, "policies/DataSecurity/*.json") : "${var.role_name_prefix}${split(".", basename(path))[0]}" => path } : {}
 
@@ -87,6 +82,7 @@ resource "aws_iam_policy" "data_security" {
   tags        = var.tags
 }
 
+# Permissions required for the `Remediation` feature
 resource "aws_iam_policy" "remediation" {
   for_each = contains(local.features, "Remediation") ? { for path in fileset(path.module, "policies/Remediation/*.json") : "${var.role_name_prefix}${split(".", basename(path))[0]}" => path } : {}
 
@@ -97,7 +93,7 @@ resource "aws_iam_policy" "remediation" {
   tags        = var.tags
 }
 
-
+# Permissions required for the `Serverless Function Scanning` feature
 resource "aws_iam_policy" "serverless_function_scanning" {
   for_each = contains(local.features, "Serverless Function Scanning") ? { for path in fileset(path.module, "policies/ServerlessFunctionScanning/*.json") : "${var.role_name_prefix}${split(".", basename(path))[0]}" => path } : {}
 
@@ -106,6 +102,13 @@ resource "aws_iam_policy" "serverless_function_scanning" {
   description = "${var.role_name_prefix}${split(".", basename(each.value))[0]}"
   policy      = file("${path.module}/${each.value}")
   tags        = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "managed_policy" {
+  for_each = { for arn in toset(local.managed_policy_arns) : "AWSManaged-${split("/", arn)[1]}" => arn }
+
+  role       = aws_iam_role.this.name
+  policy_arn = each.value
 }
 
 resource "aws_iam_role_policy_attachment" "config_scan" {
